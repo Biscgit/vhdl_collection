1321b71f03fb3cde4aa6b1ad7f25f37edbf352b5
David Horvát <horvatda@proton.me>
2025-11-10 15:36:51 +0100

    Added port for reading the internal counter

--- src/clocks/e_async_timer.vhd
+++ src/clocks/e_async_timer.vhd
@@ -19,7 +19,8 @@ entity e_async_timer is
         -- reset
         slv_reset: in std_logic;
         -- output signal
-        slv_output: out std_logic
+        slv_output: out std_logic;
+        slv_counter: out unsigned
     );
 end entity e_async_timer;
 
@@ -28,22 +29,23 @@ architecture a_async_timer of e_async_timer is
     subtype T_COUNTER is unsigned(CNT_WIDTH - 1 downto 0);
 
     constant CNT_LIMIT: T_COUNTER := resize(CLOCK_FREQUENCY * DURATION / TIME_UNIT - 1, CNT_WIDTH);
-    signal slv_counter: T_COUNTER;
+    signal slv_internal_counter: T_COUNTER;
 begin
-    p_counter: process(slv_clk, slv_reset)
+    slv_counter <= slv_internal_counter;
 
+    p_counter: process(slv_clk, slv_reset)
     begin
         -- asynchronously resetable
         if slv_reset = '0' then
-            slv_counter <= to_unsigned(1, CNT_WIDTH);
+            slv_internal_counter <= to_unsigned(0, CNT_WIDTH);
         elsif rising_edge(slv_clk) then
             slv_output <= '0';
-            if slv_counter >= CNT_LIMIT then
+            if slv_internal_counter >= CNT_LIMIT then
                 -- set to 1 to avoid skipping one clock cycle
-                slv_counter <= to_unsigned(1, CNT_WIDTH);
+                slv_internal_counter <= to_unsigned(0, CNT_WIDTH);
                 slv_output <= '1';
             else
-                slv_counter <= slv_counter + 1;
+                slv_internal_counter <= slv_internal_counter + 1;
             end if;
         end if;
     end process p_counter;
